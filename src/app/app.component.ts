ed059f76ecbdc4600413be4b863060353a4b8021
Balaji <balaji@pofitec.com>
2018-10-26 21:23:47 +0530

    Balaji_oct
    app_Creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'offer';
+  products = [
+  {
+  'id': 1,
+  'name': 'Shirt',
+  'price': '20'
+  },
+  {
+  'id': 2,
+  'name': 'Black Shirt',
+  'price': '50'
+  }
+  ]
 }
